8d5b79aa5c30417b03eb5a66961f48e29bf42eb6
chang47 <coralbue@gmail.com>
2014-12-19 20:04:42 -0800

    added load screen

---
+++ LiberationDay/Assets/StartController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartController : MonoBehaviour {
+
+
+	public void OnClickPlay() {
+		Application.LoadLevel ("LiberationDay");
+	}
+}
